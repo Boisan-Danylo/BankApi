8414e091cefd94e2f58f43e161edebe346e1e3fb
Boisan Danylo <boberiosif@gmail.com>
2025-09-03 15:38:21 +0300

    added DTOs

---
+++ src/Bank.Contracts/CreateAccountRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Contracts
+{
+    public sealed class CreateAccountRequest
+    {
+        [Required] public string OwnerName { get; set; } = default!;
+        [Range(0, double.MaxValue)] public decimal InitialBalance { get; set; }
+        public string? Currency { get; set; } = "USD";
+    }
+}
---
+++ src/Bank.Contracts/TransferRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Contracts
+{
+    public sealed class TransferRequest
+    {
+        [Required] public string FromAccount { get; set; } = default!;
+        [Required] public string ToAccount { get; set; } = default!;
+        [Range(0.01, double.MaxValue)] public decimal Amount { get; set; }
+    }
+}
