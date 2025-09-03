649663f0e788fdf3261cf0c1e1419a1bfae1da62
Boisan Danylo <boberiosif@gmail.com>
2025-09-03 16:08:27 +0300

    Added unit tests

---
+++ tests/Bank.Tests/AccountServiceTests.cs
@@ -0,0 +1,75 @@
+﻿using Bank.Application.Services;
+using Bank.Domain.Errors;
+using Bank.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace Bank.Tests
+{
+    public class AccountServiceTests
+    {
+        [Fact]
+        public void Create_ValidAccount_Succeeds()
+        {
+            var repo = new InMemoryAccountRepository();
+            var service = new AccountService(repo);
+
+            var account = service.Create("John Doe", 100, "USD");
+
+            Assert.NotNull(account);
+            Assert.Equal("John Doe", account.OwnerName);
+            Assert.Equal(100, account.Balance);
+            Assert.Equal("USD", account.Currency);
+            Assert.Equal(account, repo.Get(account.AccountNumber));
+        }
+
+        [Fact]
+        public void Create_Throws_On_EmptyOwner()
+        {
+            var repo = new InMemoryAccountRepository();
+            var service = new AccountService(repo);
+
+            Assert.Throws<ValidationException>(() => service.Create("", 100));
+            Assert.Throws<ValidationException>(() => service.Create("   ", 100));
+        }
+
+        [Fact]
+        public void Create_Throws_On_NegativeBalance()
+        {
+            var repo = new InMemoryAccountRepository();
+            var service = new AccountService(repo);
+
+            Assert.Throws<ValidationException>(() => service.Create("John Doe", -1));
+        }
+
+        [Fact]
+        public void Get_Returns_Account()
+        {
+            var repo = new InMemoryAccountRepository();
+            var service = new AccountService(repo);
+
+            var account = service.Create("Jane", 50);
+            var fetched = service.Get(account.AccountNumber);
+
+            Assert.Equal(account, fetched);
+        }
+
+        [Fact]
+        public void List_Returns_AllAccounts_Sorted()
+        {
+            var repo = new InMemoryAccountRepository();
+            var service = new AccountService(repo);
+
+            var a1 = service.Create("A", 10);
+            var a2 = service.Create("B", 20);
+
+            var list = service.List();
+
+            Assert.Equal(2, list.Count);
+            Assert.True(list.First().AccountNumber.CompareTo(list.Last().AccountNumber) < 0);
+        }
+    }
+}
---
+++ tests/Bank.Tests/AccountTransactionsServiceTests.cs
@@ -0,0 +1,106 @@
+﻿using Bank.Application.Services;
+using Bank.Domain.Errors;
+using Bank.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Tests
+{
+    public class AccountTransactionsServiceTests
+    {
+        [Fact]
+        public void Deposit_AddsToBalance()
+        {
+            var repo = new InMemoryAccountRepository();
+            var crud = new AccountService(repo);
+            var tx = new AccountTransactionsService(repo);
+
+            var acc = crud.Create("A", 100, "USD");
+            tx.Deposit(acc.AccountNumber, 50);
+
+            Assert.Equal(150, crud.Get(acc.AccountNumber).Balance);
+        }
+
+        [Fact]
+        public void Withdraw_SubtractsFromBalance()
+        {
+            var repo = new InMemoryAccountRepository();
+            var crud = new AccountService(repo);
+            var tx = new AccountTransactionsService(repo);
+
+            var acc = crud.Create("A", 100, "USD");
+            tx.Withdraw(acc.AccountNumber, 40);
+
+            Assert.Equal(60, crud.Get(acc.AccountNumber).Balance);
+        }
+
+        [Fact]
+        public void Withdraw_Throws_On_InsufficientFunds()
+        {
+            var repo = new InMemoryAccountRepository();
+            var crud = new AccountService(repo);
+            var tx = new AccountTransactionsService(repo);
+
+            var acc = crud.Create("A", 10, "USD");
+            Assert.Throws<InsufficientFundsException>(() => tx.Withdraw(acc.AccountNumber, 20));
+        }
+
+        [Fact]
+        public void Transfer_MovesFunds_BetweenAccounts()
+        {
+            var repo = new InMemoryAccountRepository();
+            var crud = new AccountService(repo);
+            var tx = new AccountTransactionsService(repo);
+
+            var a = crud.Create("A", 100, "USD");
+            var b = crud.Create("B", 50, "USD");
+
+            tx.Transfer(a.AccountNumber, b.AccountNumber, 30);
+
+            Assert.Equal(70, crud.Get(a.AccountNumber).Balance);
+            Assert.Equal(80, crud.Get(b.AccountNumber).Balance);
+        }
+
+        [Fact]
+        public void Transfer_Throws_On_SameAccount()
+        {
+            var repo = new InMemoryAccountRepository();
+            var crud = new AccountService(repo);
+            var tx = new AccountTransactionsService(repo);
+
+            var a = crud.Create("A", 100, "USD");
+
+            Assert.Throws<ValidationException>(() => tx.Transfer(a.AccountNumber, a.AccountNumber, 10));
+        }
+
+        [Fact]
+        public void Transfer_Throws_On_NonPositiveAmount()
+        {
+            var repo = new InMemoryAccountRepository();
+            var crud = new AccountService(repo);
+            var tx = new AccountTransactionsService(repo);
+
+            var a = crud.Create("A", 100, "USD");
+            var b = crud.Create("B", 50, "USD");
+
+            Assert.Throws<ValidationException>(() => tx.Transfer(a.AccountNumber, b.AccountNumber, 0));
+            Assert.Throws<ValidationException>(() => tx.Transfer(a.AccountNumber, b.AccountNumber, -5));
+        }
+
+        [Fact]
+        public void Transfer_Throws_On_CurrencyMismatch()
+        {
+            var repo = new InMemoryAccountRepository();
+            var crud = new AccountService(repo);
+            var tx = new AccountTransactionsService(repo);
+
+            var a = crud.Create("A", 100, "USD");
+            var b = crud.Create("B", 50, "EUR");
+
+            Assert.Throws<ValidationException>(() => tx.Transfer(a.AccountNumber, b.AccountNumber, 10));
+        }
+    }
+}
